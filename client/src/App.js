c0d198918297f002d0beeadc6ec2cbe95c31b79d
Ankur-Sarkar <ssarkqr@gmail.com>
2021-09-27 13:29:55 +0530

    client v2

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import { Home, NotFound } from "./Components/default";
+import Header from "./Components/Header/Header";
+import DetailView from "./Components/ItemDetails/DetailView";
+import TemplateProvider from "./templates/TemplateProvider";
+import ContextProvider from "./context/ContextProvider";
+import Cart from "./Components/Cart/Cart";
+import { Box } from "@material-ui/core";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <TemplateProvider>
+      <ContextProvider>
+        <BrowserRouter>
+          <Header />
+          <Box style={{ marginTop: 54 }}>
+            <Switch>
+              <Route exact path="/" component={Home} />
+              <Route exact path="/cart" component={Cart} />
+              {/* <Route exact path= '/product/:id' component={Product} /> */}
+              <Route exact path="/product/:id" component={DetailView} />
+              <Route component={NotFound} />
+            </Switch>
+          </Box>
+        </BrowserRouter>
+      </ContextProvider>
+    </TemplateProvider>
   );
 }
 
