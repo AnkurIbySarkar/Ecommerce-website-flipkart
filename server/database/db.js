8dccb5416a0e7dbcae8c44531dac7b3fd5ee1821
Ankur-Sarkar <ssarkqr@gmail.com>
2021-09-27 12:41:38 +0530

    First commit

---
+++ server/database/db.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const Connection = async (username, password) => {
+  const URL = `mongodb+srv://${username}:${password}@cluster0.9bmba.mongodb.net/ECOMMERCE?retryWrites=true&w=majority`;
+  try {
+    await mongoose.connect(URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Database connected successfully");
+  } catch (error) {
+    console.log("Error:", error.message);
+  }
+};
+
+export default Connection;
