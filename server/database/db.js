99aca4bf95a948f4456c110e2bc4602823fb9f10
Ankur-Sarkar <ssarkqr@gmail.com>
2021-09-29 13:32:12 +0530

    deploy

--- server/database/db.js
+++ server/database/db.js
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 
-const Connection = async (username, password) => {
-  const URL = `mongodb+srv://${username}:${password}@cluster0.9bmba.mongodb.net/ECOMMERCE?retryWrites=true&w=majority`;
+const Connection = async (URL) => {
   try {
     await mongoose.connect(URL, {
       useNewUrlParser: true,
--- server/server.js
+++ server/server.js
@@ -12,13 +12,14 @@ import Routes from "./routes/route.js";
 dotenv.config();
 const app = express();
 
-const PORT = 8000;
+const PORT = process.env.PORT || 8000;
 
 const username = process.env.DB_USERNAME;
 const password = process.env.DB_PASSWORD;
 
-Connection(username, password);
+const URL = `mongodb+srv://${username}:${password}@cluster0.9bmba.mongodb.net/ECOMMERCE?retryWrites=true&w=majority`;
 
+Connection(process.env.MONGODB_URL || URL);
 app.listen(PORT, () =>
   console.log(`Server is running successfully on PORT ${PORT}`)
 );
